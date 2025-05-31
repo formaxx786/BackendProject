b75a0f7093b09d5c2fe2f049f5cfa4966d437ac4
formaxx786 <formaxx2000@gmail.com>
2025-05-31 23:46:04 +0530

    connect to mongoAtlas

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+        
+
+        
+    } catch (error) {
+        console.error("MONGODB connection Failed", error);
+        process.exit(1); // Exit process with failure
+    }
+}
+export default connectDB;
