cca69bac61820d10022f701b04ca686beceefe3e
formaxx786 <formaxx2000@gmail.com>
2025-07-26 17:19:15 +0530

    utils code

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (requestHandler)  => {
+    return async (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+    }
+}
+
+export {asyncHandler}
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (fun) => () => {}
+// const asyncHandler = (fun) => { () => {} }
+// const asyncHandler = (fun) => async () => {} 
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next);
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: error.message || 'Internal Server Error'
+//         });
+//     }
+    
+// }
